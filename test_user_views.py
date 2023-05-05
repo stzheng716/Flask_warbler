2fd3b280021e51f8739500a731f55c6c8b27b850
Steven <stzheng716@gmail.com>
2023-05-05 14:48:55 -0700

    test log in view

--- test_user_views.py
+++ test_user_views.py
@@ -10,4 +10,54 @@ from unittest import TestCase
 
 from models import db, Message, User
 
+from app import app, CURR_USER_KEY
+
 os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+db.drop_all()
+db.create_all()
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+class UserBaseViewTestCase(TestCase):
+    def setUp(self):
+        User.query.delete()
+
+        u1 = User.signup("u1", "u1@email.com", "password", None)
+        u2 = User.signup("u2", "u2@email.com", "password", None)
+        db.session.add(u1,u2)
+        db.session.flush()
+
+        db.session.commit()
+
+        self.u1_id = u1.id
+        self.u2_id = u2.id
+
+        self.client = app.test_client()
+    
+    def tearDown(self):
+        db.session.rollback()
+
+    def test_login_and_view_follower_following(self):
+        
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+        resp = c.get(f'/users/{self.u2_id}/followers')
+
+        self.assertEqual(resp.status_code, 200)
+        html = resp.get_data(as_text=True)
+        self.assertIn("<!-- follower test block -->" ,html)
+
+        del sess[CURR_USER_KEY]
+
+        resp = c.get(f'/users/{self.u2_id}/followers', follow_redirects=True)
+
+        self.assertEqual(resp.status_code, 200)
+        html = resp.get_data(as_text=True)
+        self.assertIn("Access unauthorized." ,html)
