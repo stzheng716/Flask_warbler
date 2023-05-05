639b332831a83501c67b86c22986782f1db69180
Russell Jones <justyrones@gmail.com>
2023-05-04 19:38:55 -0600

    tesing 123

--- test_user_model.py
+++ test_user_model.py
@@ -7,6 +7,8 @@
 
 import os
 from unittest import TestCase
+from sqlalchemy.exc import IntegrityError
+
 
 from models import db, User, Message, Follow
 
@@ -52,17 +54,51 @@ class UserModelTestCase(TestCase):
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
 
-    def test_u1_following_u2_true(self):
-        """test that is_following detects when user1 is following user2"""
+    def test_is_following(self):
+        """test that is_following detects when user1 is following user2
+        and also when user1 is not following user2"""
 
-        followed_user = User.query.get_or_404(self.u2_id)
-        g.user.following.append(followed_user)
+        follower = User.query.get(self.u2_id)
+        followed_user = User.query.get(self.u1_id)
+
+        follower.following.append(followed_user)
         db.session.commit()
 
+        self.assertEqual(follower.is_following(followed_user), True)
+        self.assertEqual(followed_user.is_following(follower), False)
+
+    def test_is_following(self):
+        """test that is_following detects when user2 is following user1 and
+        when user2 is not following user1"""
+
+        follower = User.query.get(self.u1_id)
+        followed_user = User.query.get(self.u2_id)
+
+        follower.following.append(followed_user)
+        db.session.commit()
+
+        self.assertEqual(follower.is_following(followed_user), True)
+        self.assertEqual(followed_user.is_following(follower), False)
+
+    def test_User_signup(self):
+        """test that User.signup creates user when inputs are valid and doesnt
+        when inputs are invalid"""
+
+        u1 = User.query.get(self.u1_id)
+        self.assertEqual(u1.username, "u1")
+
+        #create a User with false inputs and test
+        def func1():
+            try:
+                u3 = User.signup("u1", "u1@email.com", "password", None)
+                # db.session.commit()
+                db.session.commit()
+            except IntegrityError:
+                return "blah"
+
+        # self.assertIsInstance(IntegrityError, )
+            # self.assertEqual(db.session.commit(), IntegrityError)
+            self.assertEqual(func1(), "blah")
 
 
-    def test_u1_following_u2_false(self):
 
-    def test_u2_following_u1_true(self):
-
-    def test_u2_following_u1_true(self):
