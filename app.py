6e5b36b94bc174811e65e70477d1df6d9b876154
Steven <stzheng716@gmail.com>
2023-07-21 15:50:48 -0700

    edit db in app.py

--- app.py
+++ app.py
@@ -14,10 +14,13 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
+database_url = os.environ['DATABASE_URL']
+database_url = database_url.replace('postgres://', 'postgresql://')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
